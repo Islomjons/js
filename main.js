8eb0c9bbea8b6561f9b2bf1bee5049270927a2ec
Islomjons <mukhammadalievislomjon@gmail.com>
2022-07-02 23:03:34 +0500

    js

---
+++ main.js
@@ -0,0 +1,13 @@
+var userName = "Islomjon";
+
+console.log(userName);
+
+var name = prompt("What is your name");
+var surname = prompt("What is your surname");
+var age = prompt("How old are you");
+var study = prompt("Where are you study");
+var hobby = prompt("What is your hobby");
+var phoneNumber = prompt("What is yor phone number");
+var password = prompt("What is your password");
+
+console.log(`Name: ${name}\nSurname: ${surname}\nAge: ${age}\nStudy: ${study}\nHobby ${hobby}\nPhoneNumber: ${phoneNumber}\nPassword: ${password}`)
